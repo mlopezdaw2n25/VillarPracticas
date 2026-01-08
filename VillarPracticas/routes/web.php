57a80f81594b00eb3ef88c0e66eee01ebcf0cd55
mlopezdaw2n25 <mlopez.daw2n25@lamerce.com>
2026-01-08 10:35:52 +0100

    Creacioon ruta sencilla carpeta profes y sus conrespondientes vistas

--- VillarPracticas/routes/web.php
+++ VillarPracticas/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('Profesors/profesor', function () {
+    return view('profesor');
+});
+
+Route::get('Profesors/listado', function () {
+    return view('listado');
+});
